b410e4d01cb1659aba2a6b0c225096ccc5d2529e
lugenx <hello@lugenx.com>
2023-09-27 22:12:57 -0400

    add todos

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get("/api/convert", (req, res) => {
+    //TODO: handle request here to convert
+    const result = convertHandler.convert(); //<-- should take request data
+  });
 };
