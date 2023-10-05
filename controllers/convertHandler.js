d25d3f25074ae48fe9436f83d4f383c0f5aa5083
Lugen <hello@lugen.io>
2023-10-05 18:21:34 -0400

    add methods

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,65 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+  this.getNum = function (input) {
+    let result = input.match(/\d+/g)[0];
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
+  this.getUnit = function (input) {
+    // TODO: should correctly read a fractional input with a decimal.
+    let result = input.match(/[^\d]+/g)[0];
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    const returnUnitMap = {
+      gal: "L",
+      lbs: "kg",
+      mi: "km",
+      L: "gal",
+      kg: "lbs",
+      km: "mi",
+    };
+
+    return returnUnitMap[initUnit];
+  };
+
+  this.spellOutUnit = function (unit) {
+    const spellOutMap = {
+      gal: "gallons",
+      lbs: "pounds",
+      mi: "miles",
+      L: "liters",
+      kg: "kilograms",
+      km: "kilometers",
+    };
+
+    return spellOutMap[unit];
+  };
+
+  this.convert = function (initNum, initUnits) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
+
+    const conversionMap = {
+      gal: galToL,
+      lbs: lbsToKg,
+      mi: miToKm,
+      L: galToL,
+      kg: lbsToKg,
+      km: miToKm,
+    };
+
+    return conversionMap[initUnits] * initNum;
+  };
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    const spelledOutInitUnit = this.spellOutUnit(initUnit);
+    const spelledOutReturnUnit = this.spellOutUnit(returnUnit);
+    let result = `${initNum} ${spelledOutInitUnit} converts to ${returnNum} ${spelledOutReturnUnit}`;
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
 "use strict";
 
 const expect = require("chai").expect;
+
 const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
   let convertHandler = new ConvertHandler();
 
   app.get("/api/convert", (req, res) => {
-    //TODO: handle request here to convert
-    const result = convertHandler.convert(); //<-- should take request data
+    const query = req.query;
+    const initNum = convertHandler.getNum(query.input);
+    const initUnit = convertHandler.getUnit(query.input);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+
+    const string = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    let result = { initNum, initUnit, returnNum, returnUnit, string };
+
+    res.json(result);
   });
 };
