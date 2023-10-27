69158b79785c09ff7a3529dceafcfa1c85ab960d
lugenx <hello@lugenx.com>
2023-10-26 23:44:02 -0400

    add test->should correctly read a whole number input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,11 @@
-const chai = require('chai');
+const chai = require("chai");
 let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite("Unit Tests", function () {
+  test("should correctly read a whole number input", () => {
+    assert(convertHandler.getNum("25kg"), 25);
+  });
 });
