79e7e0c9c4bcae685afe8a77ac32c78b59f5623d
lugenx <hello@lugenx.com>
2023-11-02 17:40:23 -0400

    add tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -21,10 +21,38 @@ suite("Unit Tests", function () {
     assert.strictEqual(convertHandler.getNum("7/3.14mi"), 7 / 3.14);
   });
 
-  // TODO: Fix this in code, so it can be tested with this?
-  //   test("convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3)", () => {
-  //     assert.throws(convertHandler.getNum("3/2/3l"), Error, "");
-  //   });
+  test("convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3)", () => {
+    assert.throws(
+      () => {
+        convertHandler.checkInput("3/2/3l");
+      },
+      Error,
+      /invalid number/
+    );
+  });
+
+  test("convertHandler should correctly read each valid input unit", () => {
+    const INPUT_UNITS = ["L", "l", "kg", "km", , "gal", , "lbs", , "mi"];
+    const EXPECTED_UNITS = ["L", "L", "kg", "km", , "gal", , "lbs", , "mi"];
+
+    for (let i = 0; i < INPUT_UNITS.length; i++) {
+      assert.strictEqual(
+        convertHandler.getUnit(`5${INPUT_UNITS[i]}`),
+        `${EXPECTED_UNITS[i]}`
+      );
+    }
+  });
+
+  test("convertHandler should correctly return an error for an invalid input unit", () => {
+    assert.throws(
+      () => {
+        convertHandler.checkInput("23p");
+      },
+      Error,
+      /invalid unit/
+    );
+  });
+  ///
 });
 
 /**
@@ -33,12 +61,12 @@ suite("Unit Tests", function () {
     convertHandler should correctly read a fractional input.
     convertHandler should correctly read a fractional input with a decimal.
     convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).
-    
     convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.
-
     convertHandler should correctly read each valid input unit.
     convertHandler should correctly return an error for an invalid input unit.
+
     convertHandler should return the correct return unit for each valid input unit.
+    
     convertHandler should correctly return the spelled-out string unit for each valid input unit.
     convertHandler should correctly convert gal to L.
     convertHandler should correctly convert L to gal.
