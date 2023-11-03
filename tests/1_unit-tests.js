6172a7ba586f38165dbd4e2958e1a9949689fc60
Lugen <hello@lugen.io>
2023-11-02 22:53:57 -0400

    complete unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -52,27 +52,71 @@ suite("Unit Tests", function () {
       /invalid unit/
     );
   });
-  ///
+
+  test("convertHandler should correctly read each valid input unit", () => {
+    const INPUT_UNITS = ["L", "l", "kg", "km", "gal", "lbs", "mi"];
+    const EXPECTED_UNITS = ["L", "L", "kg", "km", "gal", "lbs", "mi"];
+
+    for (let i = 0; i < INPUT_UNITS.length; i++) {
+      assert.strictEqual(
+        convertHandler.getUnit(`5${INPUT_UNITS[i]}`),
+        `${EXPECTED_UNITS[i]}`
+      );
+    }
+  });
+
+  test("convertHandler should return the correct return unit for each valid input unit", () => {
+    const INPUT_UNITS = ["L", "l", "kg", "km", "gal", "lbs", "mi"];
+    const EXPECTED_UNITS = ["gal", "gal", "lbs", "mi", "L", "kg", "km"];
+
+    for (let i = 0; i < INPUT_UNITS.length; i++) {
+      assert.strictEqual(
+        convertHandler.getReturnUnit(INPUT_UNITS[i]),
+        EXPECTED_UNITS[i]
+      );
+    }
+  });
+
+  test("convertHandler should correctly return the spelled-out string unit for each valid input unit", () => {
+    const INPUT_UNITS = ["gal", "lbs", "mi", "l", "kg", "km"];
+    const EXPECTED_UNITS = [
+      "gallons",
+      "pounds",
+      "miles",
+      "liters",
+      "kilograms",
+      "kilometers",
+    ];
+
+    for (let i = 0; i < INPUT_UNITS.length; i++) {
+      assert.strictEqual(
+        convertHandler.spellOutUnit(INPUT_UNITS[i]),
+        EXPECTED_UNITS[i]
+      );
+    }
+  });
+
+  test("convertHandler should correctly convert gal to L", () => {
+    assert.strictEqual(convertHandler.convert("5", "gal"), 18.92705);
+  });
+
+  test("convertHandler should correctly convert L to gal", () => {
+    assert.strictEqual(convertHandler.convert("6", "L"), 1.58503);
+  });
+
+  test("convertHandler should correctly convert mi to km", () => {
+    assert.strictEqual(convertHandler.convert("7", "mi"), 11.26538);
+  });
+
+  test("convertHandler should correctly convert km to mi", () => {
+    assert.strictEqual(convertHandler.convert("8", "km"), 4.97098);
+  });
+
+  test("convertHandler should correctly convert lbs to kg", () => {
+    assert.strictEqual(convertHandler.convert("9", "lbs"), 4.08233);
+  });
+
+  test("convertHandler should correctly convert kg to lbs", () => {
+    assert.strictEqual(convertHandler.convert("10", "kg"), 22.04624);
+  });
 });
-
-/**
-    convertHandler should correctly read a whole number input.
-    convertHandler should correctly read a decimal number input.
-    convertHandler should correctly read a fractional input.
-    convertHandler should correctly read a fractional input with a decimal.
-    convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).
-    convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.
-    convertHandler should correctly read each valid input unit.
-    convertHandler should correctly return an error for an invalid input unit.
-
-    convertHandler should return the correct return unit for each valid input unit.
-    
-    convertHandler should correctly return the spelled-out string unit for each valid input unit.
-    convertHandler should correctly convert gal to L.
-    convertHandler should correctly convert L to gal.
-    convertHandler should correctly convert mi to km.
-    convertHandler should correctly convert km to mi.
-    convertHandler should correctly convert lbs to kg.
-    convertHandler should correctly convert kg to lbs.
-
- */
