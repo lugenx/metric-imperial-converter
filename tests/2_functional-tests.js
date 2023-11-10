8db9eb242e9945891dd8c4936086f6e8c23612b6
Lugen <hello@lugen.io>
2023-11-09 22:07:46 -0500

    add functional test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,14 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 let assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite("Functional Tests", function () {
+  test("should convert a valid input such as 10L", async function () {
+    const res = await chai.request(server).get("/api/convert?input=10L");
+    assert.isObject(res.body);
+    assert.strictEqual(res.body.returnNum, 2.64172);
+  });
 });
