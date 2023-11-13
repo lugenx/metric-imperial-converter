0b5c126ee5a3a7a5a24ccdd1cc3b0139c65502bc
lugenx <hello@lugenx.com>
2023-11-12 19:17:07 -0500

    add functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -11,4 +11,27 @@ suite("Functional Tests", function () {
     assert.isObject(res.body);
     assert.strictEqual(res.body.returnNum, 2.64172);
   });
+
+  test("should return error with invalid unit for 32g", async function () {
+    const res = await chai.request(server).get("/api/convert?input=32g");
+    assert.equal(res.text, "invalid unit");
+  });
+
+  test("should return error with invalid number for 3/7.2/4kg", async function () {
+    const res = await chai.request(server).get("/api/convert?input=3/7.2/4kg");
+    assert.equal(res.text, "invalid number");
+  });
+
+  test("should return error with invalid number and unit for 3/7.2/4kilomegagram", async function () {
+    const res = await chai
+      .request(server)
+      .get("/api/convert?input=3/7.2/4kilomegagram");
+    assert.equal(res.text, "invalid number and unit");
+  });
+
+  test("should convert with no number such as kg", async function () {
+    const res = await chai.request(server).get("/api/convert?input=kg");
+    assert.isObject(res.body);
+    assert.strictEqual(res.body.returnNum, 2.20462);
+  });
 });
